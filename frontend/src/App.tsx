f52a8355a24424ad4a7173bba443ceb6086e655e
Niel <danielescobar492@gmail.com>
2025-11-06 17:14:49 -0500

    Further Changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,12 @@ import Input from "./components/Input";
 import { IoMdEyeOff } from "react-icons/io";
 import Post from "./components/Post";
 import TestView from "./pages/TestView";
+import LoginPageView from "./pages/LoginPageView";
 
 function App() {
   return (
     <div>
-      <TestView />
+      <LoginPageView />
     </div>
   );
 }
