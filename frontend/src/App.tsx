0fd676285d0fda049e843703ddb48018b01809fa
Paola Arraut <paoarraut05@gmail.com>
2025-11-10 09:00:06 -0500

    Added slider component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import Slider from "./components/Slider";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="p-8 bg-black text-white min-h-screen">
+      <Slider 
+        min={0} 
+        max={100} 
+        defaultValue={50}
+      />
+    </div>
+
+    
+
+  );
 }
 
 export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.tsx",
+    "./src/**/*.ts",
+    "./src/**/*.jsx",
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
