117fefb59b478c2bd229cbda80fc3941bfc45050
Paola Arraut <paoarraut05@gmail.com>
2025-11-13 11:06:51 -0500

    Added toggle component & empty profile picture base component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,22 @@
 import Slider from "./components/Slider";
-
+import Toggle from "./components/Toggle";
 function App() {
-  return (
-    <div className="content-center bg-black h-screen">
-      <Slider 
-        min={0} 
-        max={100} 
-        defaultValue={50}
-        onChange={(value) => console.log('Slider value:', value)}
-      />
-      </div>
-
-    
-
-  );
+    return (
+        <div className="content-center bg-black h-screen">
+            <Slider
+                min={0}
+                max={100}
+                defaultValue={50}
+                onChange={(value) => console.log("Slider value:", value)}
+            />
+            <div className="p-8">
+                <Toggle
+                    defaultVal={false}
+                    onChange={(val) => console.log("Toggle is:", val)}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
