faf27c317148162a32fd54073127334be0372ba3
BriTalley <talley.brie@gmail.com>
2025-11-17 14:45:05 -0500

    Added Feature: Spotify login

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import { ExpressAuth } from "@auth/express";
+import Spotify from "@auth/express/providers/spotify";
 import authRouter from "./routes/authRoute.js";
 import dotenv from "dotenv";
 
@@ -17,8 +19,20 @@ mongoose.connect(process.env.MONGO_DB)
 //middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 app.use("/api", authRouter);
+app.use("/auth/*", ExpressAuth(
+    {providers: [Spotify({
+        clientId: process.env.AUTH_SPOTIFY_ID, 
+        clientSecret: process.env.AUTH_SPOTIFY_SECRET})],
+        callbacks: {
+             async redirect() {
+                return "http://127.0.0.1:3002/dashboard";
+            },
+        }
+    }
+));
+
 
 //port
 const PORT = process.env.PORT;
