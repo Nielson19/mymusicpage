c07021e61441b3f12c269d2585a421d72b389249
llaurenstone <163789596+llaurenstone@users.noreply.github.com>
2025-11-19 15:28:11 -0500

    Changes Lauren

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,7 @@
+import DashboardView from "./pages/DashboardView";
+import LoginPageView from "./pages/LoginPageView";
 import ProfilePageView from "./pages/ProfilePageView";
+import SignupPageView from "./pages/SignupPageView";
 
 
 
