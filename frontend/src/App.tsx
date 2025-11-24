49fb768ce9371afd6159041d370c19cef9237c31
llaurenstone <163789596+llaurenstone@users.noreply.github.com>
2025-11-24 15:50:50 -0500

    added niel components to work on post

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,4 @@
-import DashboardView from "./pages/DashboardView";
-import LoginPageView from "./pages/LoginPageView";
-import ProfilePageView from "./pages/ProfilePageView";
 import TestView from "./pages/TestView";
-import GiphyPicker from "./components/GiphyPicker";
-import CreatePlaylist from "./components/CreatePlaylist";
-import CreatePost from "./components/CreatePost";
-
 function App() {
   return (
     <div>
