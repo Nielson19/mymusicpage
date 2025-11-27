f9b138ce6e835ccd47b5643a491bff9e1e986ade
ChrisSanchezDev <chrissanchezdev@gmail.com>
2025-11-27 13:47:57 -0500

    Removed: Comments from song.route.js

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -63,30 +63,3 @@ router.get('/search', async (req, res) => {
 });
 
 export default router;
-
-
-
-/*import { Router } from 'express';
-// import { } from '../src/controllers/song.controller.js
-
-const router = Router();
-
-// /api/song/dev/get, cause app.use adds api/song
-router.get('/dev/get', async (req, res) => {
-    try {
-        // Fetching iTunes Search API Data
-        const apiResponse = await fetch("https://itunes.apple.com/search?term=kanye+west+in+paris&entity=song&limit=1");
-
-        // Turn that data into a JSON
-        const data = await apiResponse.json();
-
-        // Send the JSON back
-        res.json(data);
-
-    } catch (error) {
-        console.error("Error fetching song:", error);
-        res.status(500).json({ error: "Failed to fetch song data" });
-    }
-});
-
-export default router;*/
