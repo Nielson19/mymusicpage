e31d03f41991e975da5d03d80d1d82bc995b09d2
Brie Anna <talley.brie@gmail.com>
2025-11-29 08:41:52 -0500

    revert pages to NielCode branch

--- frontend/src/pages/SignupPageView.tsx
+++ frontend/src/pages/SignupPageView.tsx
@@ -1,77 +1,18 @@
 import React, { useState } from "react";
 import ButtonComponent from "../components/ButtonComponent";
 import Input from "../components/Input";
-import axios from "axios";
-import { toast } from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
-
+import Headphoneslogo from "../assets/icons/Headphoneslogo.png";
 
 function SignupPageView() {
-{
-  const [data, setData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
-  const navigate = useNavigate();
-
-    const registerUser = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post("/api/register", {
-        email,
-        password,
-      });
-      console.log("Login successful:", response.data);
-      navigate("/login");
-    } catch (error) {
-      toast.error("Login failed. Please try again.");
-    }
-    // Implement login logic here
-  };
-  const { email, password } = data; 
   return (
-    <div className="flex flex-col items-center justify-center gap-1 min-h-screen bg-black text-white">
-      <form
-        onSubmit={registerUser}
-        className="flex p-8 flex-col justify-center items-center gap-1"
-      >
-        <h1 className="text-4xl font-bold mb-8">Register</h1>
-        <div className="mb-6">
-          <Input
-            size="MEDIUM"
-            type="email"
-            label="Email"
-            placeholder="user@example.com"
-            value={data.email}
-            color={{ PRIMARY: "#111111" }}
-            onChange={(e) => setData({ ...data, email: e.target.value })}
-          />
-        </div>
-
-        <div className="mb-7">
-          <Input
-            size="MEDIUM"
-            type="password"
-            label="Password"
-            placeholder="••••••••"
-            value={data.password}
-            color={{ PRIMARY: "#111111" }}
-            onChange={(e) => setData({ ...data, password: e.target.value })}
-          />
-        </div>
-
-        <ButtonComponent label="Register" type="submit" size="large" />
-        <div className="my-4 flex items-center gap-2">
-          <hr className="w-24 border-gray-600" />
-          <span className="text-gray-400">or</span>
-          <hr className="w-24 border-gray-600" />
-        </div>
-        
-      </form>
+    <div className="signup-page">
+      <ButtonComponent
+        size="large"
+        label="Sign Up"
+        onClick={() => console.log("Signup button clicked!")}
+      />
     </div>
   );
-  }
 }
+
 export default SignupPageView;
--- frontend/src/pages/TestView.tsx
+++ frontend/src/pages/TestView.tsx
@@ -1,22 +1,16 @@
-import React from "react";
-import MusicPlayerStatic from "../components/MusicPlayerStatic";
-import MusicPlayerFeature from "../components/MusicPlayerFeature";
 import ProfilePageView from "./ProfilePageView";
 import LoginPageView from "./LoginPageView";
-import SignupPageView from "./SignupPageView";
 import CreatePost from "../components/CreatePost";
 import CreatePlaylist from "../components/CreatePlaylist";
 import GiphyPicker from "../components/GiphyPicker";
-
+import Masonry from "../components/GeneralComp/Masonry";
 
 function TestView() {
   return (
-    // <div className="min-h-screen bg-black text-white flex flex-col w-screen h-screen justify-center items-center gap-10">
-    //   {/* <CreatePost /> */}
-    //   <CreatePlaylist />
-    //   {/* <GiphyPicker /> */}
-    // </div>
-    <LoginPageView/>
+    <div>
+      {/* <CreatePost /> */}
+      {/* <GiphyPicker /> */}
+    </div>
   );
 }
 
