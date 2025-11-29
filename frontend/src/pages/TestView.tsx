05eef86582ccc14555bb9cf48925fa4f05927949
Brie Anna <talley.brie@gmail.com>
2025-11-29 12:55:37 -0500

    sign up page edits

--- frontend/src/pages/TestView.tsx
+++ frontend/src/pages/TestView.tsx
@@ -4,10 +4,11 @@ import CreatePost from "../components/CreatePost";
 import CreatePlaylist from "../components/CreatePlaylist";
 import GiphyPicker from "../components/GiphyPicker";
 import Masonry from "../components/GeneralComp/Masonry";
-
+import SignupPageView from "./SignupPageView";
 function TestView() {
   return (
     <div>
+      <SignupPageView />
       {/* <CreatePost /> */}
       {/* <GiphyPicker /> */}
     </div>
