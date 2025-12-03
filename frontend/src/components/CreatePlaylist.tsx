418f9526a093d6e759933f76d80783661ebea75c
asad-ac <asadachau@gmail.com>
2025-12-03 16:01:35 -0500

    CreatePlaylist

--- frontend/src/components/CreatePlaylist.tsx
+++ frontend/src/components/CreatePlaylist.tsx
@@ -7,10 +7,15 @@ import image2 from '../assets/images/image2.jpg'
 import image3 from '../assets/images/image3.jpg'
 import image4 from '../assets/images/image4.jpg'
 import image5 from '../assets/images/image5.jpg'
+import image6 from "../assets/images/image6.jpg"
+import image7 from "../assets/images/image7.jpg"
+import image8 from "../assets/images/image8.jpg"
+import image9 from "../assets/images/image9.jpg"
+import image10 from "../assets/images/image10.jpg"
 
 export default function CreatePlaylist() {
 
-    const imgs = [image1,image2,image3,image4,image5]
+    const imgs = [image1,image2,image3,image4,image5,image6,image7,image8,image9,image10]
 
 
     function imgSelected() {
