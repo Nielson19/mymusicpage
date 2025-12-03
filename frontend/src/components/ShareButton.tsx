a3bc2676843d39617a964060ddf94c60cd9ca2fe
asad-ac <asadachau@gmail.com>
2025-12-03 14:03:34 -0500

    share button

--- frontend/src/components/ShareButton.tsx
+++ frontend/src/components/ShareButton.tsx
@@ -15,7 +15,7 @@ export default function ShareButton({ playlistId }: { playlistId: string }) {
     <button
       onClick={handleShare}
       className="bg-white/10 px-6 py-2 rounded-xl hover:bg-white/20 transition cursor-pointer relative">
-      Share {copied && ( <span className="absolute -top-6 left-1/2 -translate-x-1/2 bg-black text-white text-xs px-2 py-1 rounded"> Link copied</span> )}
+      Share {copied && ( <span className="absolute -top-6 left-1/2 -translate-x-1/2 bg-black text-white text-xs px-2 py-1 rounded"> Link copied!</span> )}
     </button>
   );
 }
